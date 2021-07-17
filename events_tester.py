c4c03f1ee390cd5002d0ea129237e5c8f64bc22f
Wouter Kleynen <wouterkleynen@hotmail.com>
2021-07-17 15:56:58 +0200

    Turns out the solver was slow, this is better

--- events_tester.py
+++ events_tester.py
@@ -6,34 +6,29 @@ import conversion_formulas
 ts = []
 ys = []
 
-# u_list = [0, 1, 2]
+
+u_list = [0, 1, 2]
 # breaking = False
 
 
-# Define event function and make it a terminal event
+# Stop at Z > 10
 def event(t, u, r_s):
-    # u_list.append(u[1])
-    # if u_list[-3] == u_list[-2] == u_list[-1] == u[1]:
-    #    global breaking
-    #    breaking = True
-    #    print('Yeet')
-    #    print(u[1])
+    event.terminal = True
     X, Y, Z = conversion_formulas.spherical_to_cartesian(u[1], u[2], u[3])
-    return Z - 10
+    return Z - 10.0
 
 
+# Stop if u[1] < r_s
 def event2(t, u, r_s):
-    return r_s - u[1]
-
-
-event.terminal = True
+    event2.terminal = True
+    return u[1] - r_s
 
 
 def fun3(t, variables, r_s):
     t, r, theta, phi, v_zero, v_one, v_two, v_three = variables
     d_v_zero = - r_s / (r * (r - r_s)) * v_zero * v_one
     d_v_one = - (r_s * (r - r_s) / (2.0 * r ** 3)) * v_zero ** 2 + (r_s / (2 * r * (r - r_s))) * v_one ** 2 + \
-                  (r - r_s) * (v_two ** 2 + np.sin(theta) ** 2 * v_three ** 2)
+              (r - r_s) * (v_two ** 2 + np.sin(theta) ** 2 * v_three ** 2)
     d_v_two = - (2.0 / r) * v_two * v_one + np.sin(theta) * np.cos(theta) * v_three ** 2
     d_v_three = -(2.0 / r) * v_one * v_three - 2 * (np.cos(theta) / np.sin(theta)) * v_two * v_three
     d_t = v_zero
@@ -44,14 +39,15 @@ def fun3(t, variables, r_s):
 
 
 def python_solver_with_termination(t, tend, V, r_s):
-    sol = solve_ivp(fun3, (0, tend), V, method='LSODA', events=event, args=(r_s,), t_eval=t)
+    sol = solve_ivp(fun3, (0, 2*tend), V, method='RK45', events=(event, event2), args=(r_s,), t_eval=t)
     ts.append(sol.t)
     ys.append(sol.y)
     if sol.status == 1:
-        t = sol.t[-1]
-        u = sol.y[:, -1].copy()
-        return sol.y
-    else:
-        print('lost geodesic')
+        if len(sol.t_events[0]) > 0:
+            print('Z = 10 was reached')
+            return sol.y
+        if len(sol.t_events[1]) > 0:
+            print('Fell within singularity')
+            return None
+    elif sol.status == 0:
         return None
-
--- schwarzschild_geodesic_construction.py
+++ schwarzschild_geodesic_construction.py
@@ -24,13 +24,6 @@ def create_specified_geodesic(x_start, y_start, dt, t_end, r_s):
     x_start_series_schwarzschild.append(x_start)
     y_start_series_schwarzschild.append(y_start)
     initial_schwarzschild = ccs.form_bol_four_dimensional_vector(x_start, y_start, z_start, 0, 0, 1, r_s)
-    # U = schwarzschild_numerical_solver.euler_method(initial_schwarzschild, n, dt, r_s)
-    # r_solver = U[1]
-    # theta_solver = U[2]
-    # phi_solver = U[3]
-    # print(r_solver[0], theta_solver[0], phi_solver[0])
-    # x_end, y_end, z_end = ccs.spherical_to_cartesian(r_solver[-1], theta_solver[-1], phi_solver[-1])
-    # print(x_end, y_end)
     U = odeint(schwarzschild_numerical_solver.python_solver, initial_schwarzschild, t, (r_s,))
     r_solver = U[:, 1]
     theta_solver = U[:, 2]
@@ -54,45 +47,47 @@ def create_geodesics_field(dt, t_end, r_s):
             z_start = -10
             x_start_series_schwarzschild.append(x_start)
             y_start_series_schwarzschild.append(y_start)
-            print(x_start, y_start, z_start)
+            print('x start = ' + str(x_start), 'y start = ' + str(y_start), 'z start = ' + str(z_start))
             # try:
             #     initial_schwarzschild = ccs.form_bol_four_dimensional_vector(x_start, y_start, z_start, 0, 0, 1, r_s)
             #     U = odeint(schwarzschild_numerical_solver.python_solver, initial_schwarzschild, t, (r_s,))
             #     r_solver     = U[:, 1]
-            #     if not all(r > r_s for r in r_solver):  # check if within Schwarzschild radius
-            #         continue
-            #     else:
-            #         theta_solver = U[:, 2]
-            #         phi_solver   = U[:, 3]
-            #         X, Y, Z = ccs.spherical_to_cartesian(r_solver, theta_solver, phi_solver)
-            #         x_end_series_schwarzschild.append(X[-1])
-            #         y_end_series_schwarzschild.append(Y[-1])
-            #         ax.plot3D(X, Y, Z, 'blue')
+            #     # if not all(r > r_s for r in r_solver):  # check if within Schwarzschild radius
+            #     #    continue
+            #     theta_solver = U[:, 2]
+            #     phi_solver   = U[:, 3]
+            #     X, Y, Z = ccs.spherical_to_cartesian(r_solver, theta_solver, phi_solver)
+            #     x_end_series_schwarzschild.append(X[-1])
+            #     print(X[-1], Y[-1], Z[-1])
+            #     y_end_series_schwarzschild.append(Y[-1])
+            #     ax.plot3D(X, Y, Z, 'blue')
             # except scipy.integrate.odepack.ODEintWarning:
+            #     print('INTEGRATE WARNING, GEODESIC SKIPT')
             #     continue
             # except ZeroDivisionError:
+            #     print('ZeroDivsionError, GEODESIC SKIPT')
             #     continue
             try:
                 initial_schwarzschild = ccs.form_bol_four_dimensional_vector(x_start, y_start, z_start, 0, 0, 1, r_s)
                 V = events_tester.python_solver_with_termination(t, t_end, initial_schwarzschild, r_s)
                 if V is not None:
                     r_solver     = V[1]
-                    if not all(r > r_s for r in r_solver):  # check if within Schwarzschild radius
-                        continue
-                    else:
-                        theta_solver = V[2]
-                        phi_solver   = V[3]
-                        X, Y, Z = ccs.spherical_to_cartesian(r_solver, theta_solver, phi_solver)
-                        print(X[-1], Y[-1], Z[-1])
-                        print('')
-                        x_end_series_schwarzschild.append(X[-1])
-                        y_end_series_schwarzschild.append(Y[-1])
-                        ax.plot3D(X, Y, Z, 'blue')
+                    theta_solver = V[2]
+                    phi_solver   = V[3]
+                    X, Y, Z = ccs.spherical_to_cartesian(r_solver, theta_solver, phi_solver)
+                    print('x end = ' + str(X[-1]), 'y end = ' + str(Y[-1]), 'z end = ' + str(Z[-1]))
+                    print('')
+                    x_end_series_schwarzschild.append(X[-1])
+                    y_end_series_schwarzschild.append(Y[-1])
+                    ax.plot3D(X, Y, Z, 'blue')
             except UserWarning:
+                print('calculation takes super long')
                 continue
             except ZeroDivisionError:
+                print('zero division error')
                 continue
             except ValueError:
+                print(ValueError)
                 continue
     ax.title.set_text('r_s = ' + str(r_s) + ', t = ' + str(t_end))
     plt.show()
